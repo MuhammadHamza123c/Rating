30bb3c0c0766ed5755a03b4cff2925f7073381be
Muhammad Hamza <muhammadhamzao241@gmail.com>
2025-04-15 17:18:54 +0500

    Add files via upload

---
+++ pass.py
@@ -0,0 +1,63 @@
+import os
+import streamlit as st
+import random
+
+# Elo functions
+def elo(playera, playerb):
+    return 1 / (1 + 10 ** ((playerb - playera) / 400))
+
+def elo_most(old_rating, opponent_rating, k_factor, actual_score):
+    expected_score = elo(old_rating, opponent_rating)
+    return old_rating + k_factor * (actual_score - expected_score)
+
+# Initialize session state for ratings
+if 'player_a' not in st.session_state:
+    st.session_state.player_a = 1000
+if 'player_b' not in st.session_state:
+    st.session_state.player_b = 1000
+if 'left_index' not in st.session_state:
+    st.session_state.left_index = 0
+if 'right_index' not in st.session_state:
+    st.session_state.right_index = 1
+
+# Load image paths
+folder_loc = "D:/Code/downloaded_images"
+image_list = [os.path.join(folder_loc, img) for img in os.listdir(folder_loc)]
+
+# Display columns for left and right images
+col1, _, col2 = st.columns([1, 0.1, 1])
+
+with col1:
+    st.image(image_list[st.session_state.left_index], caption="")
+    if st.button("This"):
+        # Update Elo ratings
+        st.session_state.player_a = elo_most(st.session_state.player_a, st.session_state.player_b, 32, 1)
+        st.session_state.player_b = elo_most(st.session_state.player_b, st.session_state.player_a, 32, 0)
+        # Keep Left Girl, change Right Girl
+        st.session_state.right_index = random.randint(0, len(image_list)-1)
+
+with col2:
+    st.image(image_list[st.session_state.right_index], caption="")
+    if st.button("That"):
+        # Update Elo ratings
+        st.session_state.player_a = elo_most(st.session_state.player_a, st.session_state.player_b, 32, 0)
+        st.session_state.player_b = elo_most(st.session_state.player_b, st.session_state.player_a, 32, 1)
+        # Keep Right Girl, change Left Girl
+        st.session_state.left_index = random.randint(0, len(image_list)-1)
+
+display_button=st.button("Display Result")
+if display_button:
+
+ st.markdown("### 💯 Current Ratings")
+ if st.session_state.player_a>st.session_state.player_b:
+     
+  st.image(image_list[st.session_state.left_index])
+  st.write("Girl Rating:", round(st.session_state.player_a, 2))
+
+ else:
+  st.image(image_list[st.session_state.right_index])
+ 
+  st.write("Right Girl Rating:", round(st.session_state.player_b, 2))
+
+
+
